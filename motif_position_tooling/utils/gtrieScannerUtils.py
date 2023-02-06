93bc3015cb00b6e2f500049abcb7ca6b1b6d209f
Tim Garrels <tim.garrels@protonmail.com>
2023-02-06 14:43:44 +0100

    Implement Motif Frequency Loader

--- motif_position_tooling/utils/gtrieScannerUtils.py
+++ motif_position_tooling/utils/gtrieScannerUtils.py
@@ -4,6 +4,7 @@ from subprocess import Popen, PIPE
 from pathlib import Path
 import argparse
 import zipfile
+from typing import Dict
 
 
 def gtrieScanner(graph_edgelist: Path, motif_size: int, output_directory: Path, gtrieScanner_executable: Path,
@@ -58,6 +59,38 @@ def gtrieScanner(graph_edgelist: Path, motif_size: int, output_directory: Path, 
             arcname="motif_pos",
         )
     os.remove(output_directory / str(motif_size) / 'motif_pos')
+
+
+def parse_motif_analysis_results_table(frequency_file: Path, k: int) -> Dict[str, int]:
+    """Load a motif frequency file created by gtrieScanner"""
+    with open(frequency_file, "r") as f:
+        lines = f.readlines()
+    table_lines = lines[lines.index("Motif Analysis Results\n") + 2:]
+
+    header = table_lines.pop(0)
+
+    # remove trailing newlines
+    while table_lines[-1] == "\n":
+        table_lines.pop(-1)
+
+    frequencies = {}
+    # Each table row contains a newline followed by a lien by line
+    # printed adj. matrix with the shape k*k.
+    # The last line of the matrix also contains some metrics
+    # So we process k+1 lines batches, to process each row
+    table_row_height = k + 1
+    for i in range(0, len(table_lines), table_row_height):
+        _, *motif_id_parts = table_lines[i:i + table_row_height]
+
+        # Separate last part into graph part and metric part
+        frequency = motif_id_parts[-1].split("|")[0].split(" ")[-2]
+        motif_id_parts[-1] = motif_id_parts[-1].split(" ")[0]
+
+        motif_id = " ".join(map(lambda s: s.strip(), motif_id_parts))
+
+        frequencies[motif_id] = int(frequency)
+
+    return frequencies
 
 
 if __name__ == "__main__":
