e9b97dda184357f96d4d2b67ddcc4b606cb09407
Tim Garrels <tim.garrels@protonmail.com>
2023-02-23 13:16:49 +0100

    Add Run Script for Pipeline

---
+++ pmotif_detection/main.py
@@ -0,0 +1,50 @@
+import pickle
+from pathlib import Path
+from os import makedirs
+import networkx as nx
+
+from pmotifs.PMotifGraph import PMotifGraph
+from pmotifs.config import GTRIESCANNER_EXECUTABLE
+from pmotifs.gtrieScanner.wrapper import run_gtrieScanner
+from pmotifs.positional_metrics import calculate_metrics
+
+
+def load_graph(graph_edgelist, output_directory):
+    nx_graph = nx.read_edgelist(
+        graph_edgelist,
+        create_using=nx.Graph,  # Asserts undirected graph
+        data=False,  # Asserts unweighted graph
+    )
+
+    if len(list(nx.selfloop_edges(nx_graph))) > 0:
+        raise ValueError("Graph contains Self-Loops!")  # Asserts simple graph
+
+    if min(map(int, nx_graph.nodes)) < 1:
+        raise ValueError("Graph contains node ids below '1'!")  # Assert lowest node index is >= 1
+
+    return PMotifGraph(graph_edgelist, output_directory)
+
+
+def main(edgelist, out, graphlet_size):
+    pmotif_graph = load_graph(edgelist, out)
+    run_gtrieScanner(
+        graph_edgelist=pmotif_graph.get_graph_path(),
+        gtrieScanner_executable=GTRIESCANNER_EXECUTABLE,
+        directed=False,
+        graphlet_size=graphlet_size,
+        output_directory=pmotif_graph.get_graphlet_directory(),
+    )
+
+    data = calculate_metrics(pmotif_graph, graphlet_size)
+    with open(pmotif_graph.get_graphlet_metric_file(graphlet_size), "wb") as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    GRAPH_EDGELIST = Path("/home/timgarrels/masterthesis/datasets/yeastInter_st.txt")
+    OUT = Path("/home/timgarrels/masterthesis/output/pmotif_detection/yeastInter_st")
+    makedirs(OUT, exist_ok=True)
+
+    GRAPHLET_SIZE = 4
+
+    main(GRAPH_EDGELIST, OUT, GRAPHLET_SIZE)
