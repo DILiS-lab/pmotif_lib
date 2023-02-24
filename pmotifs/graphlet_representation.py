b0ffeb7650700a8fd445a64644382808e2c9987c
Tim Garrels <tim.garrels@protonmail.com>
2023-02-24 15:00:06 +0100

    Change Order of Graphlet Class Name Definitions

--- pmotifs/graphlet_representation.py
+++ pmotifs/graphlet_representation.py
@@ -6,12 +6,12 @@ import networkx as nx
 GRAPHLET_CLASS_NAME_LOOKUP = {
     "011 101 110": "Triangle",
     "011 100 100": "3-Dash",
-    "0111 1000 1000 1000": "Fork",
     "0110 1001 1000 0100": "4-Dash",
+    "0111 1000 1000 1000": "Fork",
     "0111 1010 1100 1000": "Spoon",
+    "0110 1001 1001 0110": "Square",
     "0111 1011 1100 1100": "Crossed Square",
     "0111 1011 1101 1110": "Double Crossed Square",
-    "0110 1001 1001 0110": "Square",
 }
 
 
