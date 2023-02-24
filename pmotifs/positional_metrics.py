2b545c9510e81df48348fd1e8d2b217f16613215
Tim Garrels <tim.garrels@protonmail.com>
2023-02-24 15:42:24 +0100

    Add Smart Processing and Multiprocessing of POsitional Metrics

--- pmotifs/positional_metrics.py
+++ pmotifs/positional_metrics.py
@@ -1,14 +1,16 @@
 """This utility takes a network and nodes (or supernodes)
 and calculates various positional metrics for those inputs"""
 import statistics
-from typing import List, Callable, Dict, Iterable
+from typing import List, Callable, Dict, Tuple
 from tqdm import tqdm
+from multiprocessing import Pool
 import networkx as nx
 from networkx.algorithms.community import greedy_modularity_communities
 
 from pmotifs.GraphletOccurence import GraphletOccurrence
 from pmotifs.GraphletPositionalMetrics import GraphletPositionalMetrics, GraphPositionalMetrics
 from pmotifs.PMotifGraph import PMotifGraph
+from pmotifs.config import WORKERS
 
 
 def get_hubs(g: nx.Graph) -> List[str]:
@@ -47,13 +49,7 @@ def get_group_degree(g: nx.Graph, group: List[str]) -> int:
     return external_degree
 
 
-def graphlet_occurrence_distance_to_nodes(g: nx.Graph, anchor_nodes: List[str], group: List[str]):
-
-    nodes_shortest_path_lookup = {
-        anchor_node: nx.single_source_shortest_path_length(g, anchor_node)
-        for anchor_node in anchor_nodes
-    }
-
+def graphlet_occurrence_distance_to_nodes(g: nx.Graph, nodes_shortest_path_lookup: Dict[str, Dict[str, int]], group: List[str]):
     path_lengths = []
 
     anchor_node: str
@@ -78,6 +74,32 @@ def graphlet_occurrence_module_participation(g: nx.Graph, modules: List[List[str
     return participations
 
 
+def single_arg_wrapper_create_graphlet_positional_metrics(args):
+    return create_graphlet_positional_metrics(*args)
+
+
+def create_graphlet_positional_metrics(
+        g: nx.Graph,
+        graphlet_occurrence: GraphletOccurrence,
+        nodes_shortest_path_lookup: Dict[str, Dict[str, int]],
+        graph_modules: List[List[str]],
+) -> Tuple[GraphletOccurrence, GraphletPositionalMetrics]:
+    return (
+        graphlet_occurrence,
+        GraphletPositionalMetrics(
+            degree=get_group_degree(
+                g, graphlet_occurrence.nodes
+            ),
+            anchor_node_distances=graphlet_occurrence_distance_to_nodes(
+                g, nodes_shortest_path_lookup, graphlet_occurrence.nodes
+            ),
+            graph_module_participation=graphlet_occurrence_module_participation(
+                g, graph_modules, graphlet_occurrence.nodes
+            ),
+        ),
+    )
+
+
 def process_graphlet_occurrences(
         g: nx.Graph,
         graphlet_occurrences: List[GraphletOccurrence],
@@ -87,21 +109,27 @@ def process_graphlet_occurrences(
     """Calculate motif positional metrics"""
 
     anchor_nodes = anchor_nodes_generator(g)
+    nodes_shortest_path_lookup = {
+        anchor_node: nx.single_source_shortest_path_length(g, anchor_node)
+        for anchor_node in anchor_nodes
+    }
+
     graph_modules = graph_modules_generator(g)
 
     positional_metrics: Dict[GraphletOccurrence, GraphletPositionalMetrics] = {}
 
-    pbar_graphlet_occurrences: Iterable[GraphletOccurrence] = tqdm(
-        graphlet_occurrences,
-        desc="Calculating Positional Metrics for Graphlet Occurrences",
-        leave=False,
-    )
-    for graphlet_occurrence in pbar_graphlet_occurrences:
-        positional_metrics[graphlet_occurrence] = GraphletPositionalMetrics(
-            degree=get_group_degree(g, graphlet_occurrence.nodes),
-            anchor_node_distances=graphlet_occurrence_distance_to_nodes(g, anchor_nodes, graphlet_occurrence.nodes),
-            graph_module_participation=graphlet_occurrence_module_participation(g, graph_modules, graphlet_occurrence.nodes),
-        )
+    with Pool(processes=WORKERS) as p:
+        with tqdm(
+            total=len(graphlet_occurrences),
+            desc="Calculating Positional Metrics for Graphlet Occurrences",
+        ) as pbar:
+            for (g_oc, g_pm) in p.imap_unordered(
+                    single_arg_wrapper_create_graphlet_positional_metrics,
+                    [(g, _g_oc, nodes_shortest_path_lookup, graph_modules) for _g_oc in graphlet_occurrences],
+                    chunksize=100,
+            ):
+                positional_metrics[g_oc] = g_pm
+                pbar.update()
 
     return GraphPositionalMetrics(
         anchor_nodes=anchor_nodes,
@@ -118,7 +146,9 @@ def calculate_metrics(
     ) -> GraphPositionalMetrics:
     """When pointed to a graph and a motif file, unzips the motif file, reads the graph and calculates various
     positional metrics"""
+    print("Reading Graph")
     g = nx.readwrite.edgelist.read_edgelist(pmotif_graph.get_graph_path(), data=False, create_using=nx.Graph)
+    print("Loading Graphlet Occurrences")
     graphlet_occurrences: List[GraphletOccurrence] = pmotif_graph.load_graphlet_pos_zip(graphlet_size)
-
+    print("Processing Graphlet Occurrences")
     return process_graphlet_occurrences(g, graphlet_occurrences, anchor_nodes_generator, graph_modules_generator)
