2ec0fb3a6668665a15d1155a94faef63cb3091ee
Tim Garrels <tim.garrels@protonmail.com>
2023-04-18 14:24:06 +0200

    Add config .env loading

--- pmotif_lib/config.py
+++ pmotif_lib/config.py
@@ -1,13 +1,13 @@
 """Configuration constants."""
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-BASE_PATH = Path(__file__).absolute().parent.parent.parent.parent
-
+load_dotenv()
 
 DATASET_DIRECTORY = Path(os.getenv("DATASET_DIRECTORY"))
 EXPERIMENT_OUT = Path(os.getenv("EXPERIMENT_OUT"))
 GTRIESCANNER_EXECUTABLE = Path(os.getenv("GTRIESCANNER_EXECUTABLE"))
 
-
-WORKERS = 8
+WORKERS = int(os.getenv("WORKERS", default="1"))
